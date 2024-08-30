175f9ee97016204c223190802f284d244e54969d
tourist <17322619133@163.com>
2024-08-30 18:05:11 +0800

    feat: :sparkles: add pravite_key recover methods about random k
    https://en.wikipedia.org/wiki/Elliptic_Curve_Digital_Signature_Algorithm As the standard notes, it is not only required for k to be secret, but it is also crucial to select different k for different signatures.

--- src/crypto/asymmetric/sm2.py
+++ src/crypto/asymmetric/sm2.py
@@ -54,6 +54,8 @@ class SM2(object):
 
     def verify(self, signature: asn1_str, data: bytes, public_key: Tuple[int, int]):
         r, s = DerSequence().decode(bytes.fromhex(signature))
+        if r < 1 or r > self.curve.field.n - 1 or s < 1 or s > self.curve.field.n - 1:  # type: ignore
+            raise Exception("invalid signature")
 
         public_key_point = ec.Point(self.curve, public_key[0], public_key[1])
 
@@ -173,3 +175,34 @@ class SM2(object):
             raise Exception("invalid cipher text c3, u != c3")
 
         return plain_txt
+
+    def recover_privateKey_by_kAndrs(self, k: int, r: int, s: int):
+        if r < 1 or r > self.curve.field.n - 1 or s < 1 or s > self.curve.field.n - 1:  # type: ignore
+            raise Exception("invalid signature")
+        # d = (k-s)(r + s)^-1 mod n
+        return (k - s) * pow(r + s, -1, self.curve.field.n) % self.curve.field.n
+
+    def recover_privateKey_by_fixedk_and_2rs(self, r1: int, s1: int, r2: int, s2: int):
+        if (
+            r1 < 1
+            or r1 > self.curve.field.n - 1
+            or s1 < 1
+            or s1 > self.curve.field.n - 1
+        ):  # type: ignore
+            raise Exception("invalid signature r1 s1")
+        if (
+            r2 < 1
+            or r2 > self.curve.field.n - 1
+            or s2 < 1
+            or s2 > self.curve.field.n - 1
+        ):  # type: ignore
+            raise Exception("invalid signature r2 s2")
+        # r = x1 + e mod n
+        # k = s + (r + s) d mod n
+        # s1 + (r1 + s1) d = s2 + (r2 + s2) d mod n
+        # s1 - s2 = (r2 + s2 - r1 - s1) d mod n
+        return (
+            (s1 - s2)
+            * pow(r2 + s2 - r1 - s1, -1, self.curve.field.n)
+            % self.curve.field.n
+        )
--- tests/test_sm2.py
+++ tests/test_sm2.py
@@ -3,6 +3,7 @@ import unittest
 
 sys.path.append("./src")
 from crypto.asymmetric import sm2
+from Crypto.Util.number import bytes_to_long, long_to_bytes
 from crypto.utils.types import asn1_str
 
 
@@ -44,7 +45,7 @@ class TestSM2(unittest.TestCase):
         )
         self.assertTrue(result)
 
-    def test_sig_with_random_k(self):
+    def test_sign_with_random_k(self):
         signature = self.sm2.sign(b"hello world", self.private_key)
         result = self.sm2.verify(asn1_str(signature), b"hello world", self.public_key)
         self.assertTrue(result)
@@ -58,3 +59,30 @@ class TestSM2(unittest.TestCase):
         cipher = self.sm2.encrypt(b"hello world", self.public_key)
         res = self.sm2.decrypt(asn1_str(cipher), self.private_key)
         self.assertEqual(res, b"hello world")
+
+    def test_recover_privateKey_by_kAndrs(self):
+        r = 0x37AF670C4742BD0C8D7CF68FCEBFE61885AA630695D50A15DF279CD64327466F
+        r = bytes_to_long(long_to_bytes(r)[::-1])
+        s = 0x6701CFB5F356887B9441323FDC08FBA900E1050109FD95F024DC9C178CEBE7A4
+        s = bytes_to_long(long_to_bytes(s)[::-1])
+        k = 0xD2D569D2A7250B2B27DF909C9AFC1FD9E0A555AEC4BFB5D80CD71F70ADACF414
+        d = self.sm2.recover_privateKey_by_kAndrs(k, r, s)
+        self.assertEqual(
+            d, 0xE711E7FEE2F7DB4DE74F94B4D818718FDAF86291150227E7CB5323CDD7FF3B75
+        )
+
+    def test_recover_privateKey_by_fixedk_and_2rs(self):
+        # P = {
+        #     "x": 0xE83E542C594496D1F75A7C07841F2DE773DB59CA8A277CC77BAB2FD1BA90B858,
+        #     "y": 0x5F7CC3C9863D129D4DDFACD1B529A31CCB81463AF8A8BB5AB480A3F8BB7DA737,
+        # }
+        # e11 = 0x875817FFC25231A88B68696273AEECE852A10CCDE93C19476482EBA4D4877322
+        # e12 = 0x8FB2B63B9CF9ED7842CC0E0A204B36A3ED5C45936B6148646A26915120F6C7D2
+        r1 = 0x1260185C3D7437E6A63F1E18FD810A314A5E27D67884A83F1283D72F1009F699
+        s1 = 0x0E9F423B578A8707C83C1A0A3982F52D0FF718C2B481966E4D839CD566EE7209
+        r2 = 0x1ABAB698181BF3B65DA2C2C0AA1D53ECE519609BFAA9D75C18277CDB5C794B49
+        s2 = 0xEBB541CA42C5CCA5FA1324DDC32D3F352546FE4EECE8034E1D64A2848E2A93B9
+        d = self.sm2.recover_privateKey_by_fixedk_and_2rs(r1, s1, r2, s2)
+        self.assertEqual(
+            d, 0x3B90F86F263049ADBAE06CBB1E2F8EFEF2142F2CC4979050A3D3109DF7D83714
+        )
